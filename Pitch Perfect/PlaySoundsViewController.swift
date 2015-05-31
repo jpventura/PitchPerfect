8453dfbde409993c328173dd5081c6247e8a96b3
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-05-31 09:46:24 -0300

    3.08 Creating an Instance of AVAudioPlayer.
    Summary:
      - New instance or object class: https://goo.gl/g7snys
      - Documentation for AVAudioPlayer: https://goo.gl/XuO6OB

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+        } else {
+            println("the filePath is empty")
+        }
     }
 
     @IBAction func playSlowAudio(sender: AnyObject) {
