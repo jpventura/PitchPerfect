053dab6636753194aaaa54dc041efc9a3a13bf7b
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-05-31 14:00:08 -0300

    4.08 Playing Recorded Audio.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -19,13 +19,8 @@ class PlaySoundsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
-            var filePathUrl = NSURL.fileURLWithPath(filePath)
-            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
-            audioPlayer.enableRate = true
-        } else {
-            println("the filePath is empty")
-        }
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer.enableRate = true
     }
 
     @IBAction func playSlowAudio(sender: AnyObject) {
