524b40993c4a716893aa35fe85c6f2705874344b
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-05-31 01:22:26 -0300

    3.05 Creating a New ViewController.

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Joao Paulo Fernandes Ventura on 5/31/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
