758a82775504adfb8b99c8e4ddd1f2a63af23a2e
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-05-30 21:24:10 -0300

    1.01 Open Xcode and Create a Project.

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Joao Paulo Fernandes Ventura on 5/30/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
