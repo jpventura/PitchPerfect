2f3eeff1a6147a1313cdb63072719e551efcfd1a
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-05-31 10:03:29 -0300

    3.10 Play Audio.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -11,13 +11,15 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    var audioPlayer:AVAudioPlayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
             var filePathUrl = NSURL.fileURLWithPath(filePath)
-            var audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
         } else {
             println("the filePath is empty")
         }
