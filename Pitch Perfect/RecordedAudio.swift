808174f8aa0a3e03e4f8502e283126f3bbd4a93f
Joao Paulo Fernandes Ventura <jp.ventura@outlook.com>
2015-06-14 19:51:44 -0300

    Task 1: Create RecordedAudio initializer.
    Summary:
      - The Swift Programming Language (https://goo.gl/V8iJly)
      - Swift failable initializers (http://goo.gl/vcO1qd)

--- Pitch Perfect/RecordedAudio.swift
+++ Pitch Perfect/RecordedAudio.swift
@@ -8,9 +8,14 @@
 
 import Foundation
 
-class RecordedAudio: NSObject {
+class RecordedAudio {
 
     var filePathUrl: NSURL!
     var title: String!
+
+    init(filePathUrl: NSURL, title: String) {
+        self.filePathUrl = filePathUrl
+        self.title = title
+    }
 
 }
